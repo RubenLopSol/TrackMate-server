0c03fe83ffc34e2b986376ac3e6296252b8eeb9a
Xabier Naseem Mohammad <naseemxabier@gmail.com>
2023-03-09 16:55:55 +0100

    back end finalizado

--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, trusted } = require("mongoose");
 
 const userSchema = new Schema(
   {
@@ -13,21 +13,48 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required."],
     },
-    username: {
+    name: {
       type: String,
-      required: [true, "Name is required."],
+      /* required: [true, "Name is required."], */
+    },
+    lastname: {
+      type: String,
+      required: true,
+
+    },
+    phone: {
+      type: Number,
+      unique: true,
     },
     avatar: {
       type: String,
       default: " ",
     },
-    package: 
-       [{type: Schema.Types.ObjectId, ref:"package"}]
+    myPackages:
+      [{ type: Schema.Types.ObjectId, ref: "Package" }]
     ,
+
     isTransporter: {
       type: Boolean,
       default: false,
-    }
+    },
+    driverLicense: {
+      type: String,
+      unique: true,
+      license: {
+        type: String,
+        enum: ["B1", "B", "C1"]
+
+      },
+      image: {
+        type: String,
+      }
+    },
+    licensePlate: {
+      type: String,
+    },
+
+    transportedPackages: [{ type: Schema.Types.ObjectId, ref: "Package" }]
   },
   {
     timestamps: true,
